df80ee19f13b1b434595d32277340275e2e6009a
rhkrgusqls <k97764224@gmail.com>
2025-07-31 10:04:22 +0900

    delete

--- src/main/java/main/controller/DelStore.java
+++ src/main/java/main/controller/DelStore.java
@@ -3,10 +3,7 @@ package main.controller;
 import main.model.db.dao.AffiliationDAO;
 import main.model.db.dto.delAffilation.DeleteRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/affiliation")
@@ -15,9 +12,9 @@ public class DelStore {
     @Autowired
     private AffiliationDAO affiliationDAO;
 
-    @PostMapping("/delete")
-    public String deleteAffiliation(@RequestBody DeleteRequest request) {
-        int result = affiliationDAO.deleteAffiliation(request.getAffiliationCode());
+    @DeleteMapping("/delete")
+    public String deleteAffiliation(@RequestParam String affiliationCode) {
+        int result = affiliationDAO.deleteAffiliation(affiliationCode);
         if (result == 1) {
             return "삭제가 완료되었습니다.";
         } else {
